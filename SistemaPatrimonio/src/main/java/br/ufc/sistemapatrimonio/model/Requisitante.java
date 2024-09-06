4a1a9c35347274c57c160d3199d3d7e96ac7ebe8
Mauro Henrique <120841735+MauroHLO@users.noreply.github.com>
2024-09-06 14:19:18 -0300

    Atualizando Requisitante.java

--- SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/model/Requisitante.java
+++ SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/model/Requisitante.java
@@ -1,38 +1,33 @@
 package br.ufc.sistemapatrimonio.model;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Requisitante extends Usuario{
+public class Requisitante extends Usuario {
     private final HashMap<Integer, Bem> meusBens = new HashMap<>();
     private final HashMap<Integer, RequisicaoDeManutencao> minhasManutencoes = new HashMap<>();
 
     public Requisitante(String username, String password) {
-        this.setUsername(username);
-        this.setPassword(password);
+        super(username, password);
     }
 
-    // Métodos para solicitar manutenção de bens
     public void solicitarManutencao(Bem bem, String descricao) {
         RequisicaoDeManutencao requisicao = new RequisicaoDeManutencao(descricao);
         minhasManutencoes.put(requisicao.getId(), requisicao);
     }
 
-    // Métodos para visualizar seus bens
     public ArrayList<Bem> listarMeusBens() {
         return new ArrayList<>(meusBens.values());
     }
 
-    // Método para visualizar as requisições de manutenção feitas
     public ArrayList<RequisicaoDeManutencao> listarMinhasManutencoes() {
         return new ArrayList<>(minhasManutencoes.values());
     }
 
-    // Método para adicionar um bem ao requisitante
     public void adicionarBem(Bem bem) {
         meusBens.put(bem.getId(), bem);
     }
 
-    // Método para remover um bem do requisitante
     public void removerBem(int id) throws Exception {
         if (meusBens.containsKey(id)) {
             meusBens.remove(id);
@@ -41,4 +36,3 @@ public class Requisitante extends Usuario{
         }
     }
 }
-
