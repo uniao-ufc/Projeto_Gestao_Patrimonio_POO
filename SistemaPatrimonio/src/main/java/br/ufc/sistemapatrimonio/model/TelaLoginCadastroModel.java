a4e70dda2bf7232e80a2b17f922dd83fdda9c7dd
Mauro Henrique <120841735+MauroHLO@users.noreply.github.com>
2024-09-06 14:21:18 -0300

    Atualizando TelaLoginCadastroModel.java

--- SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/model/TelaLoginCadastroModel.java
+++ SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/model/TelaLoginCadastroModel.java
@@ -1,11 +1,11 @@
 package br.ufc.sistemapatrimonio.model;
+
 import java.util.HashMap;
 
 public class TelaLoginCadastroModel {
     private final HashMap<String, Requisitante> requisitantesMap = new HashMap<>();
     private final HashMap<String, Administrador> administradoresMap = new HashMap<>();
-    
-    // Método para autenticar um aluno
+
     public Requisitante autenticarRequisitante(String login, String senha) {
         Requisitante requisitante = requisitantesMap.get(login);
         if (requisitante != null && requisitante.getPassword().equals(senha)) {
@@ -14,7 +14,6 @@ public class TelaLoginCadastroModel {
         return null;
     }
 
-    // Método para autenticar um administrador
     public Administrador autenticarAdministrador(String login, String senha) {
         Administrador administrador = administradoresMap.get(login);
         if (administrador != null && administrador.getPassword().equals(senha)) {
@@ -23,7 +22,6 @@ public class TelaLoginCadastroModel {
         return null;
     }
 
-    // Método para cadastrar um novo aluno
     public void cadastrarRequisitante(String login, String senha) {
         if (!requisitantesMap.containsKey(login)) {
             Requisitante novoRequisitante = new Requisitante(login, senha);
@@ -33,7 +31,6 @@ public class TelaLoginCadastroModel {
         }
     }
 
-    // Método para cadastrar um novo administrador
     public void cadastrarAdministrador(String login, String senha) {
         if (!administradoresMap.containsKey(login)) {
             Administrador novoAdministrador = new Administrador(login, senha);
@@ -41,5 +38,13 @@ public class TelaLoginCadastroModel {
         } else {
             throw new RuntimeException("Administrador já cadastrado.");
         }
+    }
+
+    public HashMap<String, Requisitante> getRequisitantesMap() {
+        return requisitantesMap;
+    }
+
+    public HashMap<String, Administrador> getAdministradoresMap() {
+        return administradoresMap;
     }
 }
