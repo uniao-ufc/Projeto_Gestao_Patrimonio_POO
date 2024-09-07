fec420ba46b72eb94bb8b18bc81f9398c85ec3a7
Mauro Henrique <120841735+MauroHLO@users.noreply.github.com>
2024-09-07 18:17:23 -0300

    Update AdminBensViewController.java

--- SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/controller/AdminBensViewController.java
+++ SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/controller/AdminBensViewController.java
@@ -1,7 +1,9 @@
 package br.ufc.sistemapatrimonio.controller;
 
+import br.ufc.sistemapatrimonio.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -45,16 +47,13 @@ public class AdminBensViewController {
     private TextField txtIDEditar;
 
     @FXML
-    private TextArea txtListaPatrimonios;
+    private TextArea txtListaBens;
 
     @FXML
     private TextField txtNomeAdd;
 
     @FXML
     private TextField txtNomeEditar;
-
-    @FXML
-    private TextField txtRemover;
 
     @FXML
     private TextField txtTipoAdd;
@@ -64,32 +63,70 @@ public class AdminBensViewController {
 
     @FXML
     void adicionarBem(ActionEvent event) {
+        try {
+            int id = Integer.parseInt(txtIDAdd.getText());
+            String nome = txtNomeAdd.getText();
+            String descricao = txtDescricaoAdd.getText();
+            int depreciacao = Integer.parseInt(txtDepressiacaoAdd.getText());
+            String tipo = txtTipoAdd.getText();
 
+            Model.getInstance().getAdminBensModel().adicionarBem(id, nome, descricao, depreciacao, tipo);
+
+            Model.getInstance().mostrarPopup("Sucesso", "Bem adicionado com sucesso!", Alert.AlertType.INFORMATION);
+            atualizarListaBens();
+        } catch (NumberFormatException e) {
+            Model.getInstance().mostrarPopup("Erro", "Por favor, insira valores válidos.", Alert.AlertType.ERROR);
+        }
     }
 
     @FXML
     void editarBem(ActionEvent event) {
+        try {
+            int id = Integer.parseInt(txtIDEditar.getText());
+            String nome = txtNomeEditar.getText();
+            String descricao = txtDescricaoEditar.getText();
+            int depreciacao = Integer.parseInt(txtDepressiacaoEditar.getText());
+            String tipo = txtTipoEditar.getText();
 
+            Model.getInstance().getAdminBensModel().editarBem(id, nome, descricao, depreciacao, tipo);
+
+            Model.getInstance().mostrarPopup("Sucesso", "Bem editado com sucesso!", Alert.AlertType.INFORMATION);
+            atualizarListaBens();
+        } catch (NumberFormatException e) {
+            Model.getInstance().mostrarPopup("Erro", "Por favor, insira valores válidos.", Alert.AlertType.ERROR);
+        }
     }
 
     @FXML
     void irParaPatrimonios(ActionEvent event) {
-
+        // Implementar navegação para a tela de Patrimonios
     }
 
     @FXML
     void irParaRequisicoes(ActionEvent event) {
-
+        // Implementar navegação para a tela de Requisições
     }
 
     @FXML
     void removerBem(ActionEvent event) {
+        try {
+            int id = Integer.parseInt(txtIDAdd.getText());
 
+            Model.getInstance().getAdminBensModel().removerBem(id);
+
+            Model.getInstance().mostrarPopup("Sucesso", "Bem removido com sucesso!", Alert.AlertType.INFORMATION);
+            atualizarListaBens();
+        } catch (NumberFormatException e) {
+            Model.getInstance().mostrarPopup("Erro", "Por favor, insira um ID válido.", Alert.AlertType.ERROR);
+        }
     }
 
     @FXML
     void sair(ActionEvent event) {
-
+        // Implementar a lógica para sair da tela
     }
 
+    private void atualizarListaBens() {
+        txtListaBens.setText(Model.getInstance().getAdminBensModel().listarBens());
+    }
 }
