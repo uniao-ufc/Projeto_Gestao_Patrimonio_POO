9312a8658d6e5ede9f32da42aa0120d2e956d5a7
Mauro Henrique <120841735+MauroHLO@users.noreply.github.com>
2024-09-07 14:18:28 -0300

    Atualizando Patrimonio.java

--- SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/entities/Patrimonio.java
+++ SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/entities/Patrimonio.java
@@ -2,36 +2,8 @@ package br.ufc.sistemapatrimonio.entities;
 
 public class Patrimonio {
     private int id;
-    private String nome;
     private TipoPatrimonio tipo;
-    private Local local;
     private int numeroTombamento;
-    private boolean Alocstatus;
-
-    public Patrimonio(int id, String nome, TipoPatrimonio tipo, Local local, int numeroTombamento, boolean alocstatus) {
-        this.id = id;
-        this.nome = nome;
-        this.tipo = tipo;
-        this.numeroTombamento = numeroTombamento;
-        this.local = local;
-        Alocstatus = alocstatus;
-    }
-
-    public Patrimonio(int id, String nome, TipoPatrimonio tipo, int numeroTombamento, boolean alocstatus) {
-        this.id = id;
-        this.nome = nome;
-        this.tipo = tipo;
-        this.numeroTombamento = numeroTombamento;
-        Alocstatus = alocstatus;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     public int getId() {
         return id;
@@ -49,27 +21,11 @@ public class Patrimonio {
         this.tipo = tipo;
     }
 
-    public Local getLocal() {
-        return local;
-    }
-
-    public void setLocal(Local local) {
-        this.local = local;
-    }
-
     public int getNumeroTombamento() {
         return numeroTombamento;
     }
 
     public void setNumeroTombamento(int numeroTombamento) {
         this.numeroTombamento = numeroTombamento;
-    }
-
-    public boolean isAlocstatus() {
-        return Alocstatus;
-    }
-
-    public void setAlocstatus(boolean alocstatus) {
-        Alocstatus = alocstatus;
     }
 }
