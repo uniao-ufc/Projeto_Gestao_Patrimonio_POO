f1bb88d14b67cc035d5747ccf353fb410d1648d3
Mauro Henrique <120841735+MauroHLO@users.noreply.github.com>
2024-09-07 18:18:16 -0300

    Update RequisitanteViewController.java

--- SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/controller/RequisitanteViewController.java
+++ SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/controller/RequisitanteViewController.java
@@ -1,68 +1,41 @@
 package br.ufc.sistemapatrimonio.controller;
 
+import br.ufc.sistemapatrimonio.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 public class RequisitanteViewController {
 
     @FXML
-    private Button btnAdicionar;
+    private Button btnIrParaBens;
 
     @FXML
-    private Button btnIrParaManutencoes;
-
-    @FXML
-    private Button btnRemover;
+    private Button btnIrParaPatrimonios;
 
     @FXML
     private Button btnSair;
 
     @FXML
-    private RadioButton radioBem;
+    private TextArea txtListaRequisicoes;
 
     @FXML
-    private RadioButton radioPatrimonio;
-
-    @FXML
-    private TextField txtDescricaoAdd;
-
-    @FXML
-    private TextField txtIDAdd;
-
-    @FXML
-    private TextArea txtListaBens;
-
-    @FXML
-    private TextField txtLocalAdd;
-
-    @FXML
-    private TextField txtNomeAdd;
-
-    @FXML
-    private TextField txtRemover;
-
-    @FXML
-    void adicionarRequisicao(ActionEvent event) {
-
+    void irParaBens(ActionEvent event) {
+        // Implementar navegação para a tela de Bens
     }
 
     @FXML
-    void irParaManutencoes(ActionEvent event) {
-
-    }
-
-    @FXML
-    void removerRequisicao(ActionEvent event) {
-
+    void irParaPatrimonios(ActionEvent event) {
+        // Implementar navegação para a tela de Patrimonios
     }
 
     @FXML
     void sair(ActionEvent event) {
-
+        // Implementar a lógica para sair da tela
     }
 
+    private void atualizarListaRequisicoes() {
+        txtListaRequisicoes.setText(Model.getInstance().getRequisitanteModel().listarRequisicoes());
+    }
 }
