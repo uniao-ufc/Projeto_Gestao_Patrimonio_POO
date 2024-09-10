2a8e499af27002420c797a1ee1732ac0726c4218
Alan Victor <v0r1cth5@gmail.com>
2024-09-09 22:07:53 -0300

    comented-exceptions
    Arquivos da pasta exceptions comentados

--- SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/exceptions/BemException.java
+++ SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/exceptions/BemException.java
@@ -1,18 +1,20 @@
 package br.ufc.sistemapatrimonio.exceptions;
 
-// Define a exceção personalizada
+// define uma exceção personalizada para a classe Bem
 public class BemException extends Exception {
-    public static final int EXISTENTE = 1;
-    public static final int ERRO = 2;
-    public static final int NAO_ENCONTRADO = 3;
+    public static final int EXISTENTE = 1; // código de erro para bem existente
+    public static final int ERRO = 2; // código de erro genérico
+    public static final int NAO_ENCONTRADO = 3; // código de erro para bem não encontrado
 
-    private final int erroCode;
+    private final int erroCode; // código de erro associado à exceção
 
+    // construtor que inicializa a exceção com um código de erro e uma mensagem
     public BemException(int erroCode, String message) {
         super(message);
         this.erroCode = erroCode;
     }
 
+    // getter para o código de erro
     public int getErroCode() {
         return erroCode;
     }
--- SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/exceptions/PatrimonioException.java
+++ SistemaPatrimonio/src/main/java/br/ufc/sistemapatrimonio/exceptions/PatrimonioException.java
@@ -1,20 +1,20 @@
 package br.ufc.sistemapatrimonio.exceptions;
 
-// Define a exceção personalizada para Patrimônios
+// define a exceção personalizada para patrimônios
 public class PatrimonioException extends Exception {
-    public static final int EXISTENTE = 1;
-    public static final int ERRO = 2;
-    public static final int NAO_ENCONTRADO = 3;
+    public static final int EXISTENTE = 1; // código de erro para patrimônio existente
+    public static final int ERRO = 2; // código de erro genérico
+    public static final int NAO_ENCONTRADO = 3; // código de erro para patrimônio não encontrado
 
-    private final int erroCode;
+    private final int erroCode; // código de erro associado à exceção
 
-    // Construtor que aceita o código de erro e a mensagem
+    // construtor que aceita o código de erro e a mensagem
     public PatrimonioException(int erroCode, String message) {
         super(message);
         this.erroCode = erroCode;
     }
 
-    // Método para obter o código de erro
+    // método para obter o código de erro
     public int getErroCode() {
         return erroCode;
     }
